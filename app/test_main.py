c071cbc20fee463f279b003520a7d4cfa8b7d7dc
darkkkCoDeR <dadayaalex@gmail.com>
2026-08-27 23:42:25 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,11 +6,16 @@ import pytest
 @pytest.mark.parametrize(
     "current_rate, predicted_rate, expected",
     [
-        pytest.param(1.0, 1.06, "Buy more cryptocurrency", id="buy if more 1.05"),
-        pytest.param(1.0, 1.05, "Shouldn't buy cryptocurrency when 1.05", id="shouldn't buy if eq 1.05"),
-        pytest.param(1.0, 0.94,"Sell all your cryptocurrency", id="sell if less 0.95"),
-        pytest.param(1.0, 0.95, "Shouldn't sell cryptocurrency when 0.95", id="shouldn't sell if eq 0.95"),
-        pytest.param(1, 1, "Do nothing", id="do nothing"),
+        pytest.param(1.0, 1.06, "Buy more cryptocurrency",
+                     id="buy if more 1.05"),
+        pytest.param(1.0, 1.05, "Shouldn't buy cryptocurrency when 1.05",
+                     id="shouldn't buy if eq 1.05"),
+        pytest.param(1.0, 0.94, "Sell all your cryptocurrency",
+                     id="sell if less 0.95"),
+        pytest.param(1.0, 0.95, "Shouldn't sell cryptocurrency when 0.95",
+                     id="shouldn't sell if eq 0.95"),
+        pytest.param(1, 1, "Do nothing",
+                     id="do nothing"),
     ]
 )
 @mock.patch("app.main.get_exchange_rate_prediction")
